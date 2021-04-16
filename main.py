f89a96133cf59ea6cd258e57295f0a13260e293d
Nikita <fhgrty30@gmail.com>
2021-04-16 13:21:08 +0300

    Добавил условие в функции "index", создал новый HTML-файл

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+autlog = False
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Солнечная система', text='Планета', text1='Звезда')
+    if autlog:
+        return render_template('index_autlog.html', title='Солнечная система')
+    else:
+        return render_template('index.html', title='Солнечная система')
+
 
 
 @app.route('/neptune')
