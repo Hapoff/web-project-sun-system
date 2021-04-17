ba2971ef39820f69b48970ffc6a3f13c1f3b25e6
Nikita <fhgrty30@gmail.com>
2021-04-17 15:47:04 +0300

    Создал страницы Сатурна и Юпитера, подкорректировал базу данных

--- main.py
+++ main.py
@@ -44,5 +44,29 @@ def uranus():
                            volume=rows[0][4], distance=rows[0][5], temperature=rows[0][6])
 
 
+@app.route('/saturn')
+def saturn():
+    con = sqlite3.connect("db/Солнечная система.db")
+    cur = con.cursor()
+    rezult = cur.execute('''SELECT * FROM Объекты WHERE ID LIKE ?''', (3,))
+    rows = cur.fetchall()
+    full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'saturn.jpg')
+    return render_template('page.html', img=full_filename,
+                           title=rows[0][1], object=rows[0][1], info=rows[0][2], radius=rows[0][3],
+                           volume=rows[0][4], distance=rows[0][5], temperature=rows[0][6])
+
+
+@app.route('/jupiter')
+def jupiter():
+    con = sqlite3.connect("db/Солнечная система.db")
+    cur = con.cursor()
+    rezult = cur.execute('''SELECT * FROM Объекты WHERE ID LIKE ?''', (4,))
+    rows = cur.fetchall()
+    full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'jupiter.jpg')
+    return render_template('page.html', img=full_filename,
+                           title=rows[0][1], object=rows[0][1], info=rows[0][2], radius=rows[0][3],
+                           volume=rows[0][4], distance=rows[0][5], temperature=rows[0][6])
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
